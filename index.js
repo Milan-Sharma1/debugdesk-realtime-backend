51c494b89a51cfa59465ef0c37fcb0a628dd8c63
Milan Sharma <milansharma5933@gmail.com>
2025-01-02 16:29:51 +0530

    fix: Code cleanup, enhance CORS configuration and improve message handling with error logging

--- index.js
+++ index.js
@@ -10,48 +10,68 @@ import { ID } from "node-appwrite";
 
 const app = express();
 const server = http.createServer(app);
+const allowedOrigins = [process.env.CORS_ORIGIN];
+const isOriginAllowed = (origin) => {
+    return allowedOrigins.includes(origin);
+};
+
 const io = new Server(server, {
     connectionStateRecovery: {},
+    cors: {
+        origin: allowedOrigins,
+    },
+    allowRequest: (req, callback) => {
+        const origin = req.headers.origin;
+        if (isOriginAllowed(origin)) {
+            // Allow the connection
+            callback(null, true);
+        } else {
+            // Deny the connection
+            callback("Origin not allowed", false);
+        }
+    },
 });
 
-//TODO: add the url of render in the appwrite  and enable cors for mydomain only
+app.use(
+    cors({
+        origin: allowedOrigins,
+    })
+);
 
-app.use(cors()); // Enable CORS for all routes
 //for testing
 app.get("/hello", (req, res) => {
     res.send("Hello, world!");
 });
 
 io.on("connection", (socket) => {
-    // console.log("A user connected");
-
     socket.on("initialSetup", (connectedUserID) => {
         socket.join(connectedUserID); //to receive private msg notification a user must always in selfRoom
-        // console.log(`Socket ${socket.id} joined selfRoom ${connectedUserID}`);
     });
 
     socket.on("joinRoom", (id) => {
         socket.join(id); //either quesId or another userId to send msg in group or private
-        // console.log(`Socket ${socket.id} joined chatRoom ${id}`);
     });
     // Listen for incoming messages
     socket.on("message", async ({ id, data }) => {
-        // console.log("Received message:", data);
-        const result = await messageValidation.safeParseAsync(data);
-        if (!result.success) return;
-        const [user, response] = await Promise.all([
-            users.get(data.userid),
-            databases.createDocument(db, msg, ID.unique(), { ...data }),
-        ]);
-        if (!user || !response) return;
-        // Broadcast the message to all connected clients
-        io.to(id).to(data.userid).emit("message", data);
-    });
-
-    // Handle disconnection
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-        // Handle any cleanup if necessary
+        try {
+            // Validate
+            const result = await messageValidation.safeParseAsync(data);
+            if (!result.success) return;
+            //save msg in db
+            const [user, response] = await Promise.all([
+                users.get(data.userid),
+                databases.createDocument(db, msg, ID.unique(), { ...data }),
+            ]);
+            if (!user || !response) {
+                console.log("User or database response is missing.");
+                return;
+            }
+            // Broadcast the message to all connected clients
+            io.to(id).to(data.userid).emit("message", data);
+        } catch (error) {
+            // Handle Appwrite or other errors
+            console.error("Error processing message:", error);
+        }
     });
 });
 
